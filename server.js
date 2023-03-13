070f24a073e04af1b6a7143f15c4334d6fa72a7d
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2023-03-14 01:34:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://ezCompliance.vercel.app'],
+    origin: ['http://localhost:3000', 'https://ez-compliance.vercel.app'],
     credentials: true,
   })
 );
