f3ceac2d8f6323719522653abf867c87851442ff
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2023-03-14 02:25:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://ez-compliance.vercel.app'],
+    origin: ['http://localhost:3000', 'https://ezcompliance.vercel.app'],
     credentials: true,
   })
 );
