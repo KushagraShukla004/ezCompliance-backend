af2c6ade293264003526eb233cc306f727a918ba
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2023-04-19 01:08:47 +0530

    Resource Controller

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -66,7 +66,7 @@ const getAllUserResources = asyncHandler(async (req, res) => {
     'employee.emp_Id': emp_Id,
   }).sort('-createdAt');
 
-  //   console.log('allFormsofUser: ', allResourcesofUser);
+  // console.log('allResourcesofUser: ', allResourcesofUser);
 
   res.status(200).json(allResourcesofUser);
 });
